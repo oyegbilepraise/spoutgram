4e06009c31211b4037a51e12394e8abaa4642928
mxbit-code03 <jpenuel03@gmail.com>
2023-05-09 18:03:34 -0400

    changes

--- screens/CreateProfileScreen/CreateProfileScreen.jsx
+++ screens/CreateProfileScreen/CreateProfileScreen.jsx
@@ -276,13 +276,12 @@ const CreateProfileScreen = () => {
                   </span>
                 </div>
               </div>
-              <br />
               {/* this is the username */}
 
               {/* this is the date of birth */}
               <div className={styles.ibistro__xyz__one}>
                 <div style={{ position: "relative" }}>
-                  <input id="dob-input" type="date" autoComplete="off" name="dateOfBirth" placeholder="Date of Birth" className={styles.data_content_pass} value={formik.values.dateOfBirth} onChange={formik.handleChange("dateOfBirth")} onFocus={handleDobFocus}
+                  <input id="dob-input" type="date" autoComplete="off" name="dateOfBirth" placeholderText="Date of Birth" className={styles.data_content_pass} value={formik.values.dateOfBirth} onChange={formik.handleChange("dateOfBirth")} onFocus={handleDobFocus}
                     onBlur={handleDobBlur} />
                   <span className={styles.absolute__span}>
 
