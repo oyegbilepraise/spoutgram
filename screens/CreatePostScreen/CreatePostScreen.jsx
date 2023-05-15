50d7e6a9195a39f755a7f4c88ce0a973f92a863a
mxbit-code03 <jpenuel03@gmail.com>
2023-05-15 06:44:08 -0400

    changes

--- screens/CreatePostScreen/CreatePostScreen.jsx
+++ screens/CreatePostScreen/CreatePostScreen.jsx
@@ -73,6 +73,35 @@ const CreatePostScreen = () => {
       router.push(Routes.HOME);
     }
   });
+
+
+  //emoji show and hide handler
+  const [isEmojiOpen, setIsEmojiOpen] = useState(false);
+  const emojiRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (emojiRef.current && !emojiRef.current.contains(event.target)) {
+        setIsEmojiOpen(false);
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  function toggleEmoji() {
+    setIsEmojiOpen(prevState => !prevState);
+  }
+
+  function handleButtonClickk(event) {
+    event.stopPropagation();
+    toggleEmoji();
+  }
+
+
   return (
     <HomeLayout>
       {/* div.timeline -> middle */}
@@ -127,7 +156,7 @@ const CreatePostScreen = () => {
             </div>
           </div>
           {/* image/video */}
-          <div className={styles.media__preview} style={{ display: "" }}>
+          <div className={styles.media__preview} style={{border: "transparent"}}>
             {/*image parent container */}
             <ImagePost media={media} />
             {/*video parent container */}
@@ -310,6 +339,38 @@ const CreatePostScreen = () => {
                 </div>
               )}
             </div>
+
+
+            {/* this is for the emojis */}
+            <div>
+              <div style={{position: "relative"}}>
+                <svg className={styles.post_icon_data} onMouseDown={handleButtonClickk} fill="#00acee" width="24px" cursor="pointer"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10.5199 19.8634C10.5955 18.6615 10.8833 17.5172 11.3463 16.4676C9.81124 16.3252 8.41864 15.6867 7.33309 14.7151L8.66691 13.2248C9.55217 14.0172 10.7188 14.4978 12 14.4978C12.1763 14.4978 12.3501 14.4887 12.5211 14.471C14.227 12.2169 16.8661 10.7083 19.8634 10.5199C19.1692 6.80877 15.9126 4 12 4C7.58172 4 4 7.58172 4 12C4 15.9126 6.80877 19.1692 10.5199 19.8634ZM19.0233 12.636C15.7891 13.2396 13.2396 15.7891 12.636 19.0233L19.0233 12.636ZM22 12C22 12.1677 21.9959 12.3344 21.9877 12.5L12.5 21.9877C12.3344 21.9959 12.1677 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM10 10C10 10.8284 9.32843 11.5 8.5 11.5C7.67157 11.5 7 10.8284 7 10C7 9.17157 7.67157 8.5 8.5 8.5C9.32843 8.5 10 9.17157 10 10ZM17 10C17 10.8284 16.3284 11.5 15.5 11.5C14.6716 11.5 14 10.8284 14 10C14 9.17157 14.6716 8.5 15.5 8.5C16.3284 8.5 17 9.17157 17 10Z"></path></svg>
+                 
+                {/* this is the emoji container */}
+                {isEmojiOpen && (
+                <div className={styles.emojis__container__parent} ref={emojiRef}>
+                  <div>
+                    <div>
+                      {/* search for emojis */}
+                      <input type="search" name="" id="" placeholder="Search emojis" className={styles.search__emojis} />
+                    </div>
+                    <div style={{width: "100%", height: "105px", overflowY: "auto"}}>
+
+                      {/* shoot the emoji here */}
+                      {/* emojis */}
+
+
+                    </div>
+                  </div>
+                </div>
+                )}
+                {/* this is the emoji container */}
+
+              </div>
+            </div>
+            {/* this is for the emojis */}
+
+
             <div>
               <button
                 className={styles.data_vh_post}
