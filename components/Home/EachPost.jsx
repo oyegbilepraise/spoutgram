a273070e3ed5710ff02977f2ce6fe8537f54ec73
mxbit-code03 <jpenuel03@gmail.com>
2023-05-16 15:57:59 -0400

    changes

--- components/Home/EachPost.jsx
+++ components/Home/EachPost.jsx
@@ -106,7 +106,7 @@ const EachPost = ({ post }) => {
 
 
                                 {/* this is the green repost icon /> */}
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="repost" style={{ fill: "rgb(0, 128, 0, 0.775);" }} className={`${styles.green} ${styles.x_icn_ftr} ${styles.repostme}`} width={24} height={24} >
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="repost" style={{ fill: "rgb(0, 128, 0, 0.775)" }} className={`${styles.green} ${styles.x_icn_ftr} ${styles.repostme}`} width={24} height={24} >
                                     <path fill="none" d="M0 0h24v24H0z" />
                                     <path d="M8 20v1.932a.5.5 0 0 1-.82.385l-4.12-3.433A.5.5 0 0 1 3.382 18H18a2 2 0 0 0 2-2V8h2v8a4 4 0 0 1-4 4H8zm8-16V2.068a.5.5 0 0 1 .82-.385l4.12 3.433a.5.5 0 0 1-.321.884H6a2 2 0 0 0-2 2v8H2V8a4 4 0 0 1 4-4h10z" />
                                 </svg>
---
+++ components/svg/TypeLoader.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import styles from '@/layout/HomeLayout/HomeLayout.module.css'
+
+const TypeLoader = () => {
+    return (
+        <>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    style={{
+      margin: "auto",
+      background: "rgba(255, 255, 255, 0)",
+      display: "block",
+      shapeRendering: "auto",
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      transform : "translate(-50%, -50%)",
+    }}
+    width="32px"
+    height="32px"
+    viewBox="0 0 100 100"
+    preserveAspectRatio="xMidYMid"
+  >
+    <circle cx={84} cy={50} r={10} fill="#bebebe">
+      <animate
+        attributeName="r"
+        repeatCount="indefinite"
+        dur="0.25s"
+        calcMode="spline"
+        keyTimes="0;1"
+        values="10;0"
+        keySplines="0 0.5 0.5 1"
+        begin="0s"
+      />
+      <animate
+        attributeName="fill"
+        repeatCount="indefinite"
+        dur="1s"
+        calcMode="discrete"
+        keyTimes="0;0.25;0.5;0.75;1"
+        values="#bebebe;#bebebe;#bebebe;#bebebe;#bebebe"
+        begin="0s"
+      />
+    </circle>
+    <circle cx={16} cy={50} r={10} fill="#bebebe">
+      <animate
+        attributeName="r"
+        repeatCount="indefinite"
+        dur="1s"
+        calcMode="spline"
+        keyTimes="0;0.25;0.5;0.75;1"
+        values="0;0;10;10;10"
+        keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+        begin="0s"
+      />
+      <animate
+        attributeName="cx"
+        repeatCount="indefinite"
+        dur="1s"
+        calcMode="spline"
+        keyTimes="0;0.25;0.5;0.75;1"
+        values="16;16;16;50;84"
+        keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+        begin="0s"
+      />
+    </circle>
+    <circle cx={50} cy={50} r={10} fill="#bebebe">
+      <animate
+        attributeName="r"
+        repeatCount="indefinite"
+        dur="1s"
+        calcMode="spline"
+        keyTimes="0;0.25;0.5;0.75;1"
+        values="0;0;10;10;10"
+        keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+        begin="-0.25s"
+      />
+      <animate
+        attributeName="cx"
+        repeatCount="indefinite"
+        dur="1s"
+        calcMode="spline"
+        keyTimes="0;0.25;0.5;0.75;1"
+        values="16;16;16;50;84"
+        keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+        begin="-0.25s"
+      />
+    </circle>
+    <circle cx={84} cy={50} r={10} fill="#bebebe">
+      <animate
+        attributeName="r"
+        repeatCount="indefinite"
+        dur="1s"
+        calcMode="spline"
+        keyTimes="0;0.25;0.5;0.75;1"
+        values="0;0;10;10;10"
+        keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+        begin="-0.5s"
+      />
+      <animate
+        attributeName="cx"
+        repeatCount="indefinite"
+        dur="1s"
+        calcMode="spline"
+        keyTimes="0;0.25;0.5;0.75;1"
+        values="16;16;16;50;84"
+        keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+        begin="-0.5s"
+      />
+    </circle>
+    <circle cx={16} cy={50} r={10} fill="#bebebe">
+      <animate
+        attributeName="r"
+        repeatCount="indefinite"
+        dur="1s"
+        calcMode="spline"
+        keyTimes="0;0.25;0.5;0.75;1"
+        values="0;0;10;10;10"
+        keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+        begin="-0.75s"
+      />
+      <animate
+        attributeName="cx"
+        repeatCount="indefinite"
+        dur="1s"
+        calcMode="spline"
+        keyTimes="0;0.25;0.5;0.75;1"
+        values="16;16;16;50;84"
+        keySplines="0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1;0 0.5 0.5 1"
+        begin="-0.75s"
+      />
+    </circle>
+  </svg>
+</>
+    )
+}
+
+export default TypeLoader
