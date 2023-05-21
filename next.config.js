8ecec02e4ad15b21082873d4028d8b245af29dc6
Oyegbile Praise <oluwadhammieh@gmail.com>
2023-05-21 23:49:43 +0100

    Revert "feat: Added Messages"
    This reverts commit ca895abd7b4f14c163f0b34df03e54a1a843f754.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+require('dotenv').config()
 const nextConfig = {
   reactStrictMode: true,
+    images: {
+    domains: ['res.cloudinary.com'],
+  },
+  eslint:{
+    ignoreDuringBuilds: true,
+    ignoreDuringDevelopment: true,
+  }
+}
+
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/_next/static/sitemap.xml',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
