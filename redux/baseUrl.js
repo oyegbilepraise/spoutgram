95c7b2489c94bd6abd56ed438b1adacdaf9d33ee
Oyegbile Praise <oluwadhammieh@gmail.com>
2023-05-23 14:03:19 +0100

    Feat: Updated

--- redux/baseUrl.js
+++ redux/baseUrl.js
@@ -1,4 +1,4 @@
-export const baseUrlHU = 'https://spoutgram.onrender.com/api/v1'
+export const baseUrl = 'https://spoutgram.onrender.com/api/v1'
 export const baseUrlClient = 'http://localhost:3000'
-export const baseUrl = 'http://localhost:5050/api/v1';
+export const baseUrlHU = 'http://localhost:5050/api/v1';
 export const socketbaseUrl = 'http://localhost:5050';
