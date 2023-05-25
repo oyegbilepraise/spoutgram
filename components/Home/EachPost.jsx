c1c84dff84d16fa2a8bdf3bdcbbf192015296262
Adeoye John Oluwakayode <johnfixit293@gmail.com>
2023-05-25 10:02:54 +0100

    updated

--- components/Home/EachPost.jsx
+++ components/Home/EachPost.jsx
@@ -22,7 +22,7 @@ const EachPost = ({ post }) => {
 
   // let isLiked = post.likes.includes(user?.data?._id)
   const isBookmarked = post.bookmarks.includes(user?.data?._id)
-  const isViewed = post.view.includes(user?.data?._id)
+  const isViewed = post?.view?.includes(user?.data?._id)
 
   const handlePostView = async (inView, post, entry) => {
     if (inView && !isViewed) {
@@ -171,7 +171,7 @@ const EachPost = ({ post }) => {
                     <path d="M12 18.26l-7.053 3.948 1.575-7.928L.587 8.792l8.027-.952L12 .5l3.386 7.34 8.027.952-5.935 5.488 1.575 7.928L12 18.26zm0-2.292l4.247 2.377-.949-4.773 3.573-3.305-4.833-.573L12 5.275l-2.038 4.42-4.833.572 3.573 3.305-.949 4.773L12 15.968z" />
                   </svg>
                 </span>
-                <span className={styles._00mn_spn_cnt}>{_views.length}</span>
+                <span className={styles._00mn_spn_cnt}>{_views?.length}</span>
               </span>
             </div>
           </div>
