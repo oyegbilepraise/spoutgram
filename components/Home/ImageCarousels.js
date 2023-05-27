a7983757ae19992cd998cec996c671399f51b3e1
Egwuenu Precious <egwuenuprecious7@gmail.com>
2023-05-27 17:58:10 +0100

    fix: image grid post

--- components/Home/ImageCarousels.js
+++ components/Home/ImageCarousels.js
@@ -1,103 +1,64 @@
 import React from "react";
 import styles from "@/layout/HomeLayout/HomeLayout.module.css";
 import Image from "next/image";
-import Carousel from "react-multi-carousel";
-import people2 from "../../images/people-2.jpeg";
 
 function ImageCarousels({ postImage }) {
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1,
-      slidesToSlide: 1, // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1,
-      slidesToSlide: 1, // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1, // optional, default to 1.
-    },
-  };
   const handleImageVideodisplay = (pic, id) => {
-    const fileExtension = pic.split('.').pop().toLowerCase();
-    if (['jpg', 'jpeg', 'png'].includes(fileExtension)) {
+    const fileExtension = pic.split(".").pop().toLowerCase();
+    const itemCount = postImage.length;
+
+    if (["jpg", "jpeg", "png"].includes(fileExtension)) {
       return (
-        <div>
+        <div key={id}>
           <Image
             src={pic !== null && pic}
             alt="picgrid"
-            className={''}
+            className={getItemClassName(itemCount)}
             width={200}
             height={200}
             priority
-            key={id}
           />
         </div>
-      )
-    } else if (fileExtension === 'mp4') {
+      );
+    } else if (fileExtension === "mp4") {
       return (
-        <div>
-          <video controls width={200}
-            height={200} priority
-            key={id} >
+        <div key={id}>
+          <video
+            controls
+            width={200}
+            height={200}
+            className={getItemClassName(itemCount)}
+          >
             <source src={pic !== null && pic} type="video/mp4" />
           </video>
         </div>
-      )
+      );
     }
-  }
+  };
+
+  const getItemClassName = (itemCount) => {
+    if (itemCount === 1) {
+      return "singleItem";
+    } else if (itemCount === 2) {
+      return "secondStyle";
+    } else if (itemCount === 3) {
+      return "thirdStyle";
+    } else if (itemCount === 4) {
+      return "fourthStyle";
+    }
+    return "";
+  };
+
   return (
-    <>
-      <div className={`grid ${postImage.length > 4 ? 'four-columns' : 'two-columns'}`}>
-        {postImage.map((pic, id) => (
-          <div className="grid-item">
-            {handleImageVideodisplay(pic, id)}
-          </div>
-        ))}
-      </div>
-    </>
-    // <div className={`${styles.data_content_img} ${styles.grid_101}`}>
-    //   {postImage.length === 1 ? (
-    //     <Image
-    //       src={postImage[0]}
-    //       alt="picgrid"
-    //       className={styles._00img_data}
-    //       width={1000}
-    //       height={1000}
-    //       priority
-    //     />
-    //   ) : (
-    //     <Carousel
-    //       swipeable={true}
-    //       draggable={true}
-    //       arrows={true}
-    //       showDots={true}
-    //       className=""
-    //       responsive={responsive}
-    //       ssr={true}
-    //       keyBoardControl={true}
-    //       customTransition="all .5"
-    //       transitionDuration={500}
-    //       dotListClass="custom-dot-list-style"
-    //     >
-    //       {postImage.map((pic,id) => (
-    //         <Image
-    //           src={pic!==null&&pic}
-    //           alt="picgrid"
-    //           className={styles._00img_data}
-    //           width={1000}
-    //           height={1000}
-    //           priority
-    //           key={id}
-    //         />
-    //       ))}
-    //     </Carousel>
-    //   )}
-    // </div>
+    <div
+      className={`imageContainer ${
+        postImage.length === 3 ? "thirdImageContainer" : getItemClassName(postImage.length)
+      }`}
+    >
+      {postImage.map((pic, id) => (
+        <div key={id}>{handleImageVideodisplay(pic, id)}</div>
+      ))}
+    </div>
   );
 }
 
